86d49b49ad4dacb786d528fc76a1285480edc0a3
baraabebo10 <141871147+baraabebo10@users.noreply.github.com>
2024-08-13 11:49:45 +0300

    Create solution(Lord_Baraa).cpp

---
+++ dp (easy)/c/solution(Lord_Baraa).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define nl "\n"
+#define yes "YES"
+#define no "NO"
+#define all(v) v.begin(),v.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+int main() {
+    fast
+    ll n;
+    cin>>n;
+    vector<ll>v(n);
+    for(ll &i:v)cin>>i;
+    ll MAX=(1<<10);
+    vector<vector<ll>>dp(2,vector<ll>(MAX+1,1e9));
+    for(ll i=0;i<=MAX;i++)dp[1][i]=0;
+    for(ll i=n-1;i>=0;i--){
+        for(ll j=MAX-1;j>=0;j--){
+            dp[0][j]=dp[0][j+1];
+            dp[0][j]=min(dp[0][j],dp[1][j]+__builtin_popcount(v[i]^j));
+        }
+        dp[1]=dp[0];
+    }
+    cout<<dp[0][0];
+    return 0;
+}
